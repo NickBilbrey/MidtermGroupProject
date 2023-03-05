c89c3d1eb6c5d4b35290c7c921e7fb773745e835
asmarazaq <45542493+asmarazaq@users.noreply.github.com>
2023-03-05 14:23:42 -0500

    Added cart class and animations, also made some changes to teh existing classes (#4)
    Co-authored-by: Asma Razaq <asmarazaq@rocketmortgage.com>

---
+++ MidTermGroupProject/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTermGroupProject
+{
+    public class Cart
+    {
+        public List<Product> Items = new List<Product>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            Items.Add(product);            
+        }
+        
+        public decimal GetSalesTax()
+        {           
+
+            decimal taxRate = 0.06m;           
+
+            return taxRate;
+        }
+       
+        public void Clear()
+        {
+            Items.Clear();
+        }
+    }
+}
