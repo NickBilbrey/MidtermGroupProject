4a83017a6bb0eab731732aa48e5c8629de15249f
NickBilbrey <NicholasBilbrey18@gmail.com>
2023-03-09 20:29:09 -0500

    Nicks branch (#14)
    * Initial outline
    
    * basic payment type classes
    
    * Created Menu class
    
    * need to pull
    
    * Changed the variables to properties as well as reorganized some code blocks.
    
    * Changed the credit class and properties
    
    * moved the saving of file to the file operator class, changed window width, and added method to save new items to menu

--- MidTermGroupProject/Menu.cs
+++ MidTermGroupProject/Menu.cs
@@ -28,35 +28,6 @@ namespace MidTermGroupProject
             Items.Add(new Product("Salad", "Food", "Mixed greens with balsamic vinaigrette", 5.00m, 0));
             Items.Add(new Product("Fruit Cup", "Food", "Assorted seasonal fruit", 3.50m, 0));
             Items.Add(new Product("Yogurt Parfait", "Food", "Greek yogurt with granola and berries", 4.50m, 0));
-            
-
-
-            if (!File.Exists(path))
-            {
-                // Create a file to write to.
-                using (StreamWriter sw = File.CreateText(@"C:\Temp\menu1.txt"))
-                {
-                    foreach (Product prod in Items)
-                    {
-                        sw.WriteLine($"{prod.Name},'||',{prod.Category},'||',{prod.Description},'||',{prod.Price},'||',{prod.Quantity}");                        
-
-                    }
-
-                }
-            }
-
-            // Open the file to read from.
-            using (StreamReader sr = File.OpenText(@"C:\Temp\menu1.txt"))
-            {
-                string s;
-                while ((s = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(s);
-                    
-                }
-
-            }
-
 
         }
         public void DisplayProductList()
