4d129352ee816088ca0c6c91be86325b48c08971
Asma Razaq <asmarazaq@rocketmortgage.com>
2023-03-09 20:29:52 -0500

    added separate class

--- MidTermGroupProject/Credit.cs
+++ MidTermGroupProject/Credit.cs
@@ -6,7 +6,6 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
 namespace MidTermGroupProject
 {
     public class Credit : Payment
@@ -14,14 +13,13 @@ namespace MidTermGroupProject
         public string CreditCardNumber { get; set; }
         public string ExpirationDate { get; set; }
         public string CVV { get; set; }
-
         public bool IsCardNumberValid()
         {
             if (Regex.IsMatch(CreditCardNumber, @"^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|6(?:011|5[0-9]{2})[0-9]{12})$"))
             {
                 return true;
             }
-                return false;
+            return false;
         }
         public bool IsExpirationDateValid()
         {
@@ -29,7 +27,7 @@ namespace MidTermGroupProject
             {
                 return true;
             }
-                return false;
+            return false;
         }
         public bool IsCvvValid()
         {
@@ -37,10 +35,7 @@ namespace MidTermGroupProject
             {
                 return true;
             }
-                return false;
+            return false;
         }
-
-
-
     }
 }
